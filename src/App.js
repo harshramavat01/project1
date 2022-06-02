aabf6f37ab049fcad653fa3d926dcd7c421a9ffb
harshramavat01 <harshramavat01@gmail.com>
2022-06-02 16:22:50 +0530

    table

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Gt from './components/Gt';
 
+//1 st task
+
 function App() {
   let Medicine =
     [
@@ -47,15 +49,60 @@ function App() {
       }
     ];
 
+  //employedata
+
+  const Employedata = [
+    {
+      name: "amit",
+      age: 35,
+      salary: 40000,
+      bonus: 1000,
+      status: true
+    },
+    {
+      name: "ajay",
+      age: 25,
+      salary: 38000,
+      bonus: 2000,
+      status: false
+    },
+    {
+      name: "mayur",
+      age: 23,
+      salary: 50000,
+      bonus: 500,
+      status: true
+    },
+    {
+      name: "jay",
+      age: 29,
+      salary: 35000,
+      bonus: 600,
+      status: true
+    },
+    {
+      name: "raj",
+      age: 33,
+      salary: 22000,
+      bonus: 2000,
+      status: true
+    },
+  ]
+
   // Medicine.map((m) => {
   //   for (let k in m) {
   //     console.log(m[k]);
   //   }
   // });
 
+  let Medicinem = Medicine.filter((d) => d.status === true && d.expiry >= 2022);
+  let totalPrice = Medicinem.reduce((acc, d, i) => acc + d.price, 0);
+  let Employ = Employedata.filter((e, i) => e.status === true);
+  let total= Employ.reduce((acc, e, i) => acc + e.salary + e.bonus, 0);
+
 
   return (
-    <>
+    <div>
       <table border="1">
 
         <tr>
@@ -65,28 +112,58 @@ function App() {
           <td>price</td>
           <td>expiry</td>
           <td>status</td>
+          <td>total</td>
         </tr>
         {
-          Medicine.map((d, i) => {
+          Medicinem.map((d, i) => {
+            let { id, name, quantity, price, expiry, status} = d
             return (
               <tr key={i}>
-                <td>{d.id}</td>
-                <td>{d.name}</td>
-                <td>{d.quantity}</td>
-                <td>{d.price}</td>
-                <td>{d.expiry}</td>
-                <td>{d.status.toString()}</td>
+                <td>{id}</td>
+                <td>{name}</td>
+                <td>{quantity}</td>
+                <td>{price}</td>
+                <td>{expiry}</td>
+                <td>{status.toString()}</td>
+                {i === 0 ? <td rowSpan="2">{totalPrice}</td> : null}
 
               </tr>
             )
-          }
-          )
-
-
-
+          })
         }
       </table>
-    </>
+
+      <table border="1">
+      <tr>
+          <td>Name</td>
+          <td>Age</td>
+          <td>Salary</td>
+          <td>Bonus</td>
+          <td>Status</td>
+          <td>Total bonus</td>
+          <td>total expences</td>
+        </tr>
+      {
+        Employ.map((e, i) => {
+          let { name, age, salary, bonus, status} = e
+          return (
+            <tr key={i}>
+              <td>{name}</td>
+              <td>{age}</td>
+              <td>{salary}</td>
+              <td>{bonus}</td>
+              <td>{status.toString()}</td>
+              <td>{salary + bonus}</td>
+              {i === 0 ? <td rowSpan="4">{total}</td> : null}
+
+            </tr>
+          )
+        })
+      }
+    </table>
+      </div>
+      
+      
   );
 }
 
